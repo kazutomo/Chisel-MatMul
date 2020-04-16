9db191ab4fc3b1ef3141d11da7ac4a06aced2b54
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-04-15 22:35:33 -0500

    wrote PE

--- ProcElem.scala
+++ ProcElem.scala
@@ -10,9 +10,23 @@ import chisel3.util.log2Ceil
 
 class ProcElem(val ninbits:Int = 8) extends Module {
     val io = IO(new Bundle {
-      val in  = Input(UInt(ninbits.W))
-      val out = Output(UInt(ninbits.W))
+      val in_h  = Input(UInt(ninbits.W))
+      val in_v  = Input(UInt(ninbits.W))
+      val out_h = Output(UInt((ninbits*2).W))
+      val out_v = Output(UInt((ninbits*2).W))
+      val out   = Output(UInt((ninbits*2).W))
     })
 
-  io.out := io.in
+  val res  = RegInit(0.U((ninbits*2).W))
+  val hreg = RegInit(0.U(ninbits.W))
+  val vreg = RegInit(0.U(ninbits.W))
+
+  res := res + (io.in_h * io.in_v)
+
+  hreg := io.in_h
+  vreg := io.in_v
+
+  io.out_h := hreg
+  io.out_v := vreg
+  io.out := res
 }
--- ProcElemTest.scala
+++ ProcElemTest.scala
@@ -9,7 +9,21 @@ import chisel3._
 import chisel3.iotesters.PeekPokeTester
 
 class ProcElemUnitTester(c: ProcElem) extends PeekPokeTester(c) {
-  poke(c.io.in, 10)
-  val out = peek(c.io.out)
-  println(s"ProcElem: $out")
+
+  val hins = List(1,2,3,0)
+  val vins = List(4,5,6,0)
+
+  for (i <- vins zip hins) {
+    val h = i._1
+    val v = i._2
+    poke(c.io.in_h, h)
+    poke(c.io.in_v, v)
+    val out_h = peek(c.io.out_h)
+    val out_v = peek(c.io.out_v)
+    val out = peek(c.io.out)
+
+    println(s"h=$h v=$v out_h=$out_h out_v=$out_v out=$out")
+
+    step(1)
+  }
 }
