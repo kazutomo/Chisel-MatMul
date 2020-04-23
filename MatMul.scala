31699380d3deb4cac1b0a936df65da19b34aad7a
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-04-22 21:26:01 -0500

    MatMul test passed (n=3)

--- MatMul.scala
+++ MatMul.scala
@@ -8,11 +8,39 @@ package matmul
 import chisel3._
 import chisel3.util.log2Ceil
 
-class MatMul(val ninbits:Int = 8) extends Module {
-    val io = IO(new Bundle {
-      val in  = Input(UInt(ninbits.W))
-      val out = Output(UInt(ninbits.W))
-    })
+// Compute A * B, where A and B are a square matrix.
+//
+class MatMul(val n:Int = 3, val ninbits:Int = 8) extends Module {
+  val io = IO(new Bundle {
+    val in_a  = Input(Vec(n, UInt(ninbits.W))) // horizontal inputs
+    val in_b  = Input(Vec(n, UInt(ninbits.W))) // vertical inputs
+    val out   = Output(Vec(n*n, UInt((ninbits*2).W)))
+  })
 
-  io.out := io.in
+  val p_elems = VecInit(Seq.fill(n*n) { Module(new ProcElem(ninbits)).io })
+  val h_wires = Wire(Vec((n-1)*n, UInt(ninbits.W)))
+  val v_wires = Wire(Vec(n*(n-1), UInt(ninbits.W)))
+
+  def gethidx(r: Int, c: Int) : Int = r*(n-1) + c  // last column is terminated
+  def getvidx(r: Int, c: Int) : Int = r*n + c
+
+  // connecting PEs in a systolic manner
+  for (col <- 0 until n) {
+    for (row <- 0 until n) {
+      val pidx = row*n + col
+      io.out(pidx) := p_elems(pidx).out // results
+
+      // wiring up PEs
+      // horizontal inputs
+      if (col == 0)   p_elems(pidx).in_h := io.in_a(row)
+      else            p_elems(pidx).in_h := h_wires(gethidx(row,col-1))
+      // horizontal outputs to next PEs
+      if (col < n-1)  h_wires(gethidx(row,col)) := p_elems(pidx).out_h
+      // vertical inputs
+      if (row == 0)   p_elems(pidx).in_v := io.in_b(col)
+      else            p_elems(pidx).in_v := v_wires(getvidx(row-1,col))
+      // vertical outputs to next PEs
+      if (row < n-1)  v_wires(getvidx(row,col)) := p_elems(pidx).out_v
+    }
+  }
 }
--- MatMulTest.scala
+++ MatMulTest.scala
@@ -9,7 +9,70 @@ import chisel3._
 import chisel3.iotesters.PeekPokeTester
 
 class MatMulUnitTester(c: MatMul) extends PeekPokeTester(c) {
-  poke(c.io.in, 10)
-  val out = peek(c.io.out)
-  println(s"ProcElem: $out")
+
+  def mmul(a: Array[Array[Int]], b: Array[Array[Int]]) : Array[Array[Int]] = {
+    for(r <- a) yield {
+      for(c <- b.transpose) yield r zip c map Function.tupled(_*_) reduceLeft (_+_)
+    }
+  }
+
+  def matInit(n: Int) : Array[Array[Int]] = {
+    val rseed = 100
+    val maxval = 5
+    val rnd = new scala.util.Random(rseed)
+
+    Array.tabulate(n){ _ => Array.tabulate(n){_ => rnd.nextInt(maxval+1)+1}}
+  }
+
+  def printmat(m: Array[Array[Int]]) {
+    m.foreach{r => r.foreach{v => print(f"$v%4d")} ; print(" ;") }
+    println()
+  }
+
+  val n = c.n
+  val a = matInit(n)
+  val b = a
+  val y = mmul(a,b)
+
+  printmat(a)
+  printmat(y)
+
+  def checkresult() : List[Int] = {
+    val ret = for(j <- 0 until n*n) yield {
+      val out = peek(c.io.out(j))
+      print(out + " ")
+      out.toInt
+    }
+    println()
+    ret.toList
+  }
+
+  for (clk <- 0 until 2*n) {
+    for (idx <- 0 until n) {
+      val p = clk - idx
+      if (p >= 0 && p < n) {
+        poke(c.io.in_a(idx), a(idx)(p))
+        poke(c.io.in_b(idx), b(p)(idx))
+      } else {
+        poke(c.io.in_a(idx), 0)
+        poke(c.io.in_b(idx), 0)
+      }
+    }
+    step(1)
+    print(f"$clk: ")
+    checkresult()
+  }
+  step(1)
+  print(f"${2*n}: ")
+  val output = checkresult()
+
+  var invalidcnt = 0
+  for (i <- output zip y.flatten.toList) {
+    if( i._1 != i._2 ) {
+      println("Error: " + i._1 + " " + i._2)
+      invalidcnt += 1
+    }
+  }
+
+  if (invalidcnt == 0) println("Verification passed!")
 }
